cbae55cc8f0dfd454c2266c5a50b5694fca78ef0
Ishan Pranav <ishan.pranav@outlook.com>
2024-12-01 23:05:46 -0500

    Add project

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# monopoly
-Investigation into Monopoly: The Mega Edition
+<!-- README.md -->
+<!-- Copyright (c) 2024 Ishan Pranav -->
+<!-- Licensed under the MIT license. -->
+
+# Monopoly
+
+Recently, a friend acquired a _Monopoly: The Mega Edition_ set. To prepare for
+the inevitable gameplay session, I am investigating the optimal strategy for
+both the traditional Monopoly game and this variant, with an emphasis on the
+differences between the two games.
+
+## Probabilities
+
+First, we seek the probabilities of landing on each square during the game. We
+define a _turn_ to be the time spent by a player during their move, which may
+include multiple successive dice _rolls_.
+
+To determine the probability distribution, we simulate many, many turns, and
+note the frequency of each square at the end of each roll.
---
+++ src/Monopoly/Monopoly.Simulator/Program.cs
@@ -0,0 +1,49 @@
+﻿// Program.cs
+// Copyright (c) 2024 Ishan Pranav. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Monopoly.Simulator;
+
+internal static class Program
+{
+    private static void PrintUsage()
+    {
+        Console.WriteLine("Usage: Monopoly.Simulator <turns> <seed>");
+    }
+
+    private static void Main(string[] args)
+    {
+        if (args.Length <= 0 || !int.TryParse(args[0], out int turns))
+        {
+            PrintUsage();
+
+            return;
+        }
+
+        Random random;
+
+        if (args.Length > 1)
+        {
+            if (!int.TryParse(args[1], out int seed))
+            {
+                PrintUsage();
+
+                return;
+            }
+
+            random = new Random(seed);
+        }
+        else
+        {
+            random = Random.Shared;
+        }
+
+        for (; turns > 0; turns--)
+        {
+            int a = random.Next(1, 7);
+            int b = random.Next(1, 7);
+
+            Console.WriteLine($"{a} {b}");
+        }
+    }
+}
